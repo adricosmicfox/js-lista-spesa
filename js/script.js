a29559f1f226f1d4de1e2ef90b2f4cedf85643ab
adricosmicfox <adriiphone@gmail.com>
2022-09-03 16:52:56 +0200

    created html and js, completed exercise

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Consegna:
+// Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+// Consigli:
+// - Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+// - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+
+
+
+// creare lista della spesa con oggetti
+
+let spesa = ["fragole", "melone", "pomodori", "pistacchi", "patate"];
+
+
+// creare il ciclo per la stampa degli elementi all'interno dell'array
+
+let i = 0;
+
+while (i < spesa.length) {
+    console.log(spesa[i]);
+    document.getElementById("lista-spesa").innerHTML = document.getElementById("lista-spesa").innerHTML + " " + spesa[i];
+    i++;
+}
+
